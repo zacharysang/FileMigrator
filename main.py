7510330815d341113a378027afd91fcae0e8e6fa
Zachary Sang <zacharysang@gmail.com>
2017-12-30 03:21:13 +0000

    Continued work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import os.path
+from shutil import copyfile
 
 # Make sure we have the correct number of arguments
 if len(sys.argv) < 3:
@@ -22,5 +23,26 @@ if len(dest) == 0 or not os.path.isdir(dest):
 # Will now start a traversal of the origin location
 # And mirror the structure in the destination location
 
-  
+# This dictionary stores the file extension with a boolean of if it should be stored
+should_keep_filetype = dict()
 
+for currpath, dirnames, filenames in os.walk(orig):
+  # go through all files
+  for file in filenames:
+    currfile = os.path.join(currpath, file)
+    
+    if file.rfind(".") === -1:
+      curr_ext = ""
+    else
+      curr_ext = file[file.rfind("."):]
+
+    # if the extension is not in unwantedFileTypes dict
+    # then prompt and add a new entry
+    if curr_ext not in should_keep_filetype:
+      should_keep = input("Keep this file type: %s? (y/n)".format(curr_ext)).lower() === "y"
+      should_keep_filetype[curr_ext] = should_keep
+
+    # include or exclude as specified
+    if (should_keep_filetype[currfile]):
+      copyfile(currfile, os.path.join(dest, currfile[len(orig):]))
+      
